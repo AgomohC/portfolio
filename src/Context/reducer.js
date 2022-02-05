3f53d94587065d6244c4acf3a0aadb95695b311a
AgomohC <chinaemerem98@yahoo.com>
2022-02-05 20:19:20 +0100

    added open_menu and close_menu reducers

--- src/Context/reducer.js
+++ src/Context/reducer.js
@@ -1,5 +1,13 @@
+import { OPEN_MENU, CLOSE_MENU } from "./actions";
+
 const reducer = (state, action) => {
-   switch (action) {
+   switch (action.type) {
+      case OPEN_MENU:
+         return { ...state, isMenuOpen: true };
+      case CLOSE_MENU:
+         return { ...state, isMenuOpen: false };
+      default:
+         return state;
    }
 };
 export default reducer;
