28fede551b924fffe29b40209033595c8b6d4868
AgomohC <chinaemerem98@yahoo.com>
2022-02-07 00:16:09 +0100

    started project and contact section

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -1,7 +1,24 @@
 import React from "react";
+import { Grid, makeStyles, Typography } from "@material-ui/core";
+
+const useStyles = makeStyles((theme) => ({
+   container: {
+      minHeight: "100vh",
+      paddingTop: theme.spacing(5),
+      paddingRight: theme.spacing(5),
+      paddingLeft: theme.spacing(5),
+
+      display: "flex",
+      alignItems: "center",
+      [theme.breakpoints.up("sm")]: {
+         paddingLeft: theme.spacing(10),
+      },
+   },
+}));
 
 const Contact = () => {
-   return <section></section>;
+   const classes = useStyles();
+   return <section id="contact" className={classes.container}></section>;
 };
 
 export default Contact;
--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -1,7 +1,23 @@
 import React from "react";
+import { Grid, makeStyles, Typography } from "@material-ui/core";
 
+const useStyles = makeStyles((theme) => ({
+   container: {
+      minHeight: "100vh",
+      paddingTop: theme.spacing(5),
+      paddingRight: theme.spacing(5),
+      paddingLeft: theme.spacing(5),
+
+      display: "flex",
+      alignItems: "center",
+      [theme.breakpoints.up("sm")]: {
+         paddingLeft: theme.spacing(10),
+      },
+   },
+}));
 const Projects = () => {
-   return <section></section>;
+   const classes = useStyles();
+   return <section id="contact" className={classes.container}></section>;
 };
 
 export default Projects;
